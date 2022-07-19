510e237e27d6b6b16208044f32c6842e151bcbd7
Leo <carleo1002@hotmail.com>
2022-07-19 07:58:37 -0500

    Configuracion inicial de carpetas clean architecture

---
+++ src/main/java/com/co/ias/HourCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.co.ias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HourCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HourCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/ias/HourCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.ias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HourCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
